d9fe00bfed3b15db55d1d54b999a5caeec3652a3
ddoavoca <ddoavoca@gmail.com>
2025-01-05 17:42:38 +0900

    250105 feat: Redis 접근 함수 및 키 템플릿 관리 로직 구현
    Redis 데이터 관리를 위한 접근 함수와 키 템플릿 설정 로직.
    - `.env` 파일 로드:
      - Redis 호스트, 포트, DB 번호 및 키 템플릿 환경변수에서 로드.
    - 방 관련 함수:
      - `add_to_room`, `remove_from_room`, `get_room_clients` 구현.
    - 미팅룸 관련 함수:
      - `add_to_meeting_room`, `remove_from_meeting_room`, `get_meeting_room_clients`, `delete_meeting_room` 구현.
    - 클라이언트 관련 함수:
      - `set_client_info`, `get_client_info`, `delete_client_info` 구현.

---
+++ core/redis.py
@@ -0,0 +1,67 @@
+import redis.asyncio as redis
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Redis 호스트, 포트, DB 번호
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PORT = int(os.environ['REDIS_PORT'])
+REDIS_DB = int(os.environ['REDIS_DB'])
+
+redis_client = None
+
+# Redis 키 템플릿
+ROOMS_KEY_TEMPLATE = os.environ['ROOMS_KEY_TEMPLATE']
+CLIENT_KEY_TEMPLATE = os.environ['CLIENT_KEY_TEMPLATE']
+DISCONNECTED_CLIENT_KEY_TEMPLATE = os.environ['DISCONNECTED_CLIENT_KEY_TEMPLATE']
+MEETING_ROOM_KEY_TEMPLATE = os.environ['MEETING_ROOM_KEY_TEMPLATE']
+
+# Redis 초기화 함수
+async def init_redis():
+    global redis_client
+    try:
+        redis_client = redis.Redis(
+            host=REDIS_HOST,
+            port=REDIS_PORT,
+            db=REDIS_DB,
+            decode_responses=True
+        )
+        if await redis_client.ping():
+            print("Redis connected")
+    except Exception as e:
+        print(f"Failed to connect to Redis: {e}")
+        redis_client = None
+
+# 방 관련 함수들
+async def add_to_room(room_id, client_id):
+    await redis_client.sadd(ROOMS_KEY_TEMPLATE.format(room_id=room_id), client_id)
+
+async def remove_from_room(room_id, client_id):
+    await redis_client.srem(ROOMS_KEY_TEMPLATE.format(room_id=room_id), client_id)
+
+async def get_room_clients(room_id):
+    return await redis_client.smembers(ROOMS_KEY_TEMPLATE.format(room_id=room_id))
+
+# 미팅룸 관련 함수들
+async def add_to_meeting_room(room_id, client_id):
+    await redis_client.sadd(MEETING_ROOM_KEY_TEMPLATE.format(room_id=room_id), client_id)
+
+async def remove_from_meeting_room(room_id, client_id):
+    await redis_client.srem(MEETING_ROOM_KEY_TEMPLATE.format(room_id=room_id), client_id)
+
+async def get_meeting_room_clients(room_id):
+    return await redis_client.smembers(MEETING_ROOM_KEY_TEMPLATE.format(room_id=room_id))
+
+async def delete_meeting_room(room_id):
+    await redis_client.delete(MEETING_ROOM_KEY_TEMPLATE.format(room_id=room_id))
+
+# 클라이언트 관련 함수들
+async def set_client_info(client_id, info):
+    await redis_client.hset(CLIENT_KEY_TEMPLATE.format(client_id=client_id), mapping=info)
+
+async def get_client_info(client_id):
+    return await redis_client.hgetall(CLIENT_KEY_TEMPLATE.format(client_id=client_id))
+
+async def delete_client_info(client_id):
+    await redis_client.delete(CLIENT_KEY_TEMPLATE.format(client_id=client_id))
